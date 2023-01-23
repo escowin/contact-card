ccee9763cd2558bc270d9a48c52351b0daae0173
escowin <edwin@escowinart.com>
2023-01-22 20:18:11 -0600

    19.3.5 | rendered gets & enabled form w/ post functionality

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,52 +1,57 @@
-import { openDB } from 'idb';
-import { async } from 'regenerator-runtime';
+import { openDB } from "idb";
+import { async } from "regenerator-runtime";
 import "regenerator-runtime/runtime";
 
 // async crud
 // - creates the contact_db database in indexDB
 export const initdb = async () => {
   // creates a new database named 'contact_db' which will be using version 1 of the database.
-  openDB('contact_db', 1, {
+  openDB("contact_db", 1, {
     // Adds database schema if it has not already been initialized.
     upgrade(db) {
-      if (db.objectStoreNames.contains('contacts')) {
-        console.log('contacts store already exists');
+      if (db.objectStoreNames.contains("contacts")) {
+        console.log("contacts store already exists");
         return;
       }
       // Creates a new object store for the data and give it a key name of 'id' which will increment automatically
-      db.createObjectStore('contacts', { keyPath: 'id', autoIncrement: true });
-      console.log('contacts store created');
-    }
-  })
-}
-
+      db.createObjectStore("contacts", { keyPath: "id", autoIncrement: true });
+      console.log("contacts store created");
+    },
+  });
+};
 
 // - read | get contact_db database
 export const getDb = async () => {
-  console.log('GET database');
+  // console.log('GET database');
   // indexDB database connection with specified version
-  const contactDb = await openDB('contact_db', 1);
+  const contactDb = await openDB("contact_db", 1);
   // transaction with specified store & data privileges (readme | getting privileges)
-  const tx = contactDb.transaction('contacts', 'readonly');
+  const tx = contactDb.transaction("contacts", "readonly");
   // specified object store to be opened up
-  const store = tx.objectStore('contacts');
+  const store = tx.objectStore("contacts");
   // get request object to get all the specified data above from the database
   const request = store.getAll();
   // request result
   const result = await request;
 
-  console.log('result.value', result);
+  console.log("get | contacts from indexb database", result);
   return result;
-}
+};
 
 // - post
-export const postDB = async (name, email, phone, profile) => {
-  console.log('POST database')
-  const contactDb = await openDB('contact_db', 1);
-  const tx = contactDb.transaction('contacts', 'readwrite'); // readwrite | posting privileges
-  const store = tx.objectStore('contacts');
-  const request = store.add({ name: name, email: email, phone: phone, profile: profile }); // post request object with specified key values
+export const postDb = async (name, email, phone, profile) => {
+  const contactDb = await openDB("contact_db", 1);
+  // readwrite | posting privileges
+  const tx = contactDb.transaction("contacts", "readwrite");
+  const store = tx.objectStore("contacts");
+  // post request object w/ specified key values
+  const request = store.add({
+    name: name,
+    email: email,
+    phone: phone,
+    profile: profile,
+  });
   const result = await request;
-  console.log('data saved to database', result)
 
-}
+  console.log("post | saved contact to indexdb database", result);
+};
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,14 +1,14 @@
 // entry point | webpack will include the imported files into the bundle.
 // - javascript
-import "./form";
-import "./submit";
+import { toggleForm, clearForm } from "./form";
+// import "./submit";
+
+// - bootstrap
+import { Tooltip, Toast, Popover } from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // - css
 import "../css/index.css";
-
-// bootstrap
-import { Tooltip, Toast, Popover } from "bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 // - images
 import Logo from "../images/logo.png";
@@ -17,16 +17,52 @@ import Dog from "../images/dog.png";
 // import Logo from "../images/escowinart.png";
 
 // - database crud
-import { initdb, getDb, postDB } from "./database";
+import { initdb, getDb, postDb } from "./database";
+import { fetchCards } from "./cards";
 
-// - dom manipulation
-window.addEventListener("load", function() {
-    initdb();
-    getDb();
-    postDB("user", "user@test.com", "5125551122", "Dog");
-    getDb();
+// - on-load functionality
+window.addEventListener("load", function () {
+  initdb();
+  fetchCards(); // data is pulled from the indexdb database. creates & renders new card per data object
+  // getDb();
+  // postDB();
 
-    this.document.getElementById('logo').src = Logo;
-    this.document.getElementById('bearThumbnail').src = Bear;
-    this.document.getElementById('dogThumbnail').src = Dog;
+  this.document.getElementById("logo").src = Logo;
+  this.document.getElementById("bearThumbnail").src = Bear;
+  this.document.getElementById("dogThumbnail").src = Dog;
+});
+
+// - form functionality
+const form = document.getElementById("formToggle");
+const newContactButton = document.getElementById("new-contact");
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener("click", (event) => {
+  toggleForm();
+});
+
+form.addEventListener("submit", (event) => {
+  // Handle data
+  event.preventDefault();
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let email = document.getElementById("email").value;
+  let profile = document.querySelector('input[type="radio"]:checked').value;
+
+  // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+  if (submitBtnToUpdate == false) {
+    postDb(name, email, phone, profile);
+  } else {
+    fetchCards();
+    // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
 });
