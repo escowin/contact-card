3d25b2398fd81e928b5f2eedeabba63bdf38fab6
escowin <edwin@escowinart.com>
2023-01-23 20:58:00 -0600

    19.5.4 | set up installation functionality

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -112,3 +112,21 @@ if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("./service-worker.js");
   });
 }
+
+// - installing the pwa
+const installBtn = document.getElementById('installBtn');
+
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  installBtn.style.visibility = 'visible';
+
+  installBtn.addEventListener('click', () => {
+    event.prompt();
+    installBtn.setAttribute('disabled', true);
+    installBtn.textContent = 'Installed!';
+  });
+});
+
+window.addEventListener('appinstalled', (event) => {
+  console.log('gg', 'appinstalled', event);
+})
